0fd8b5888df1b8ec14f8c8def1161f773c80a0ec
JuanGTZ07 <al45677@un.edu.mx>
2025-10-28 17:25:14 -0600

    Update comprar.js

--- JavaScript/comprar.js
+++ JavaScript/comprar.js
@@ -3,3 +3,27 @@ let IntervaloContador;
 
 let minutos = document.getElementById("min");
 let segundos = document.getElementById("seg");
+
+function ContadorInicial() {
+    var DuracionContador = 300;
+    var TiempoTranscurrido = Date.now();
+    var TiempoFaltante = DuracionContador - TiempoTranscurrido;
+
+    var minutos = Math.floor(TiempoFaltante / 1000 / 60);
+    var segundos = Math.floor((TiempoFaltante / 1000) % 60);
+
+    var Contador = document.getElementById('timer');
+    Contador.textContent = minutos + ":" + segundos;
+}
+
+window.addEventListener('load', function() {
+    ContadorInicial();
+});
+
+const StartButton = document.getElementById('start-button');
+StartButton.disabled = true;
+
+function ActualizarContador() {
+    const now = new Date().getTime();
+    const distance = 
+}
